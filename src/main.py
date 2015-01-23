c8dfeb5eb8384bd3501d652f2b94c7e9e1607f86
Corentin Smith <corentin.smith@gmail.com>
2015-01-23 11:03:59 +0100

    Update server code

--- src/main.py
+++ src/main.py
@@ -50,14 +50,26 @@ class Server:
         name = "vampygarou"
         self._send_message("NME", len(name), name)
 
-    def get_message(self):
+    def get_order(self):
+        return self.get_message(3)
+
+    def get_messages_int(self, message_count, size=1):
+        if message_count == 1:
+            return struct.unpack('=B', self.get_message(size))[0]
+        else:
+            return (struct.unpack('=B', self.get_message(size))[0] for _ in range(message_count))
+
+    def get_message(self, size):
         try:
-            order = self._sock.recv(3)
-            return order
+            message = self._sock.recv(size)
+            return message
         except Exception as e:
             raise Exception("Couldn't receive message: {}".format(e))
 
         return None
+
+    def send_move(self, *args):
+        self._send_message("MOV", *args)
 
     def close(self):
         self._sock.close()
@@ -96,39 +108,39 @@ if __name__ == "__main__":
     # main loop
     while True:
         print "Getting order"
-        order = server.get_message()
+        order = server.get_order()
         print order
 
         if order == "SET":
-            lignes, colonnes = (struct.unpack('=B', self._s.recv(1))[0] for i in range(2))
+            lignes, colonnes = server.get_messages_int(2)
             # ici faire ce qu'il faut pour préparer votre représentation
             # de la carte
         elif order == "HUM":
-            n = struct.unpack('=B', self._s.recv(1))[0]
+            n = server.get_messages_int(1)
             maisons = []
             for i in range(n):
-                maisons.append((struct.unpack('=B', self._s.recv(1))[0] for i in range(2)))
+                maisons.append(server.get_messages_int(2))
             # maisons contient la liste des coordonnées des maisons
             # ajoutez votre code ici
         elif order == "HME":
-            x, y = (struct.unpack('=B', self._s.recv(1))[0] for i in range(2))
+            x, y = server.get_messages_int(2)
             # ajoutez le code ici (x,y) étant les coordonnées de votre
             # maison
         elif order == "UPD":
-            n = struct.unpack('=B', self._s.recv(1))[0]
+            n = server.get_messages_int(1)
             changes = []
             for i in range(n):
-                changes.append((struct.unpack('=B', self._s.recv(1))[0] for i in range(5)))
+                changes.append(server.get_messages_int(5))
             # mettez à jour votre carte à partir des tuples contenus dans changes
             # calculez votre coup
             # préparez la trame MOV ou ATK
             # Par exemple:
-            send(sock, "MOV", 1, 2, 1, 1, 3)
+            server.send_move(1, 2, 1, 1, 3)
         elif order == "MAP":
-            n = struct.unpack('=B', self._s.recv(1))[0]
+            n = server.get_messages_int(1)
             changes = []
             for i in range(n):
-                changes.append((struct.unpack('=B', self._s.recv(1))[0] for i in range(5)))
+                changes.append(server.get_messages_int(5))
             # initialisez votre carte à partir des tuples contenus dans changes
         elif order == "END":
             pass
