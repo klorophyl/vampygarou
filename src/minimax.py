4e475a53626d8801d2948c7b93903c69c6e13c70
Adrien Bruchet <adrien.bruchet.gt@gmail.com>
2015-02-27 08:22:51 +0100

    clean

--- src/minimax.py
+++ src/minimax.py
@@ -1,33 +1,8 @@
-#  friends et foes sont stockés dans des dictionnaires de type
-#  {num_of_team:[coords,number_of_people]}
-
-
-class data_of_map(object):
-    def __init__(self, friends, foes):
-        super(data_of_map, self).__init__()
-        self.friends = friends
-        self.foes = foes
-
-
-# successors sera un dictionnaire avec en clé le numero de l'action
-# (type bouger, splitter, etc...) et l'état correspondant à l'action
-
-class State(object):
-    def __init__(self, abs, ord, number_of_people, succ):
-        super(State, self).__init__()
-        self.coord = [abs, ord]
-        self.number_of_people = number_of_people
-        self.successors = succ
-
-    def act(action):
-        self.coord = successors[action].coord
-        self.number_of_people = successors[action].number_of_people
-        self.successors = successors[action].successors
-        return successors[action]
-
 
 #  heuristique va utiliser les datas de la map pour générer
 #  une valeur pour chaque état
+
+
 def heuristique(state):
     pass
 
@@ -44,7 +19,7 @@ def max_value(state):
         return heuristique
 
 
-def mon_value():
+def min_value():
     pass
 
 
