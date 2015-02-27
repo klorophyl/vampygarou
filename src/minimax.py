6cf98b672866ff7c94589ccad675081a100f9c90
Adrien Bruchet <adrien.bruchet.gt@gmail.com>
2015-02-27 09:30:41 +0100

    correct typo

--- src/minimax.py
+++ src/minimax.py
@@ -23,33 +23,33 @@ def get_result(action, state):
 
 
 def max_value(state):
-    if terminal_test(state):
-        return utility(state)
+    if is_terminal_test(state):
+        return get_utility(state)
     else:
         v = float("inf")*-1
-        succ = successors(state)
+        succ = get_successors(state)
         for action_state in succ:
             v = max(v, min_value(action_state[0]))
     return v
 
 
 def min_value(state):
-    if terminal_test(state):
-        return utility(state)
+    if is_terminal_test(state):
+        return get_utility(state)
     else:
         v = float("inf")
-        succ = successors(state)
+        succ = get_successors(state)
         for action_state in succ:
             v = min(v, max_value(action_state[0]))
     return v
 
 
 def minimax(state):
-    actions = actions(state)
+    actions = get_actions(state)
     v = float("inf")*-1
     for act in actions:
-        if min_value(result(act, state)) > v:
-            v = min_value(result(act, state))
+        if min_value(get_result(act, state)) > v:
+            v = min_value(get_result(act, state))
             act_to_play = act
     return act_to_play
 
